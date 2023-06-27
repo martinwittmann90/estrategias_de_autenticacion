336f930224110a48a3772eae9a9ca8bc8ef2f16f
Martin Wittmann <107895957+martinwittmann90@users.noreply.github.com>
2023-06-27 19:28:33 -0300

    siguiendo

--- src/routes/sessions.routes.js
+++ src/routes/sessions.routes.js
@@ -16,27 +16,19 @@ sessionsRouter.post("/login", passport.authenticate("login", { failureRedirect: 
     if (!req.user) {
         return res.status(400).json({ error: "Invalid Credentials" });
     }
-    const { _id, email, firstName, lastName, age, role } = req.user;
-    req.session.user = { _id, email, firstName, lastName, role, age };
+    const { _id, email, firstName, lastName, role } = req.user;
+    req.session.user = { _id, email, firstName, lastName, role };
     return res.status(200).json({ status: "success", message: "User logged in successfully", payload: req.user });
 });
 sessionsRouter.get("/faillogin", (req, res) => {
     return res.status(400).json({ status: "error", message: "Wrong user or password" });
 });
-// sessionsRouter.post("/login", async (req, res) => {
-//   const { email, password } = req.body;
-//   const response = await usersService.login(email, password);
-//   // if login success, save user in session and redirect to products page
-//   if (response.code === 200) {
-//     req.session.user = response.result.payload;
-//   }
-//   return res.status(response.code).json(response.result);
-// });
 
 sessionsRouter.get("/github", passport.authenticate("github", { scope: ["user:email"] }));
+
 sessionsRouter.get("/githubcallback", passport.authenticate("github", { failureRedirect: "/login" }), (req, res) => {
     req.session.user = req.user;
-    res.redirect("/");
+    res.redirect("/products");
 });
 
 sessionsRouter.get("/logout", async (req, res) => {
